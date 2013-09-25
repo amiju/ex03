2574426742e291a7a4220a34d9c04f2d8c473bdb
Dzenan Ridjanovic <dzenanr@gmail.com>
2013-09-25 10:01:44 -0400

    add doc; format code

--- bin/exo03.dart
+++ bin/exo03.dart
@@ -1,17 +1,19 @@
 void main() {
   for (var i = 1; i < 101; i++) {
-   var fizz = i % 3 == 0;
-   var buzz = i % 5 == 0;
-   var fizzBuzz =(i % 3 == 0) && (i % 5 == 0);
-   if ((i % 3 == 0) && (i % 5 == 0)) {
-     print('$i: fizz buzz');
-   } else if (i % 3 == 0) {
-     print('$i: fizz');
-   } else if (i % 5 == 0){
-     print('$i: buzz');
-   } else{
-     print(i);
-   }
+    /*
+    var fizz = i % 3 == 0;
+    var buzz = i % 5 == 0;
+    var fizzBuzz =(i % 3 == 0) && (i % 5 == 0);
+    */
+    if ((i % 3 == 0) && (i % 5 == 0)) {
+      print('$i: fizz buzz');
+    } else if (i % 3 == 0) {
+      print('$i: fizz');
+    } else if (i % 5 == 0){
+      print('$i: buzz');
+    } else{
+      print(i);
+    }
   }
 }
 
